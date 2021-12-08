2bf41a018de1ed2a21b032346b3c793d31c64065
Rob M <mrrobinhood5@gmail.com>
2021-12-07 22:48:19 -0600

    Part 1 and 2 complete

--- 2021/Day 1/solution.py
+++ 2021/Day 1/solution.py
@@ -1,6 +1,5 @@
 with open('input.txt') as f:
-    raw_data = f.readlines()
-    raw_data = [int(d.rstrip()) for d in raw_data]
+    raw_data = [int(d.rstrip()) for d in f.readlines()]
 
 
 def sonar_sweep(data, count=0):
@@ -10,14 +9,14 @@ def sonar_sweep(data, count=0):
     return count
 
 
-def sonar_sweep_window(data):
-    sums = []
+def sonar_sweep_window(data, sums=[]):
     for i, d in enumerate(data):
         if i == len(data) - 2:
             break
         else:
-            sums.append(sum(data[:i+2]))
-    return sums
+            sums.append(sum(data[i:i+3]))
+    return sonar_sweep(sums)
 
-print(sonar_sweep(raw_data))
-print(sonar_sweep_window(raw_data))
+
+print(f'Part 1: {sonar_sweep(raw_data)}')
+print(f'Part 2: {sonar_sweep_window(raw_data)}')
--- 2021/Day 1/test_solution.py
+++ 2021/Day 1/test_solution.py
@@ -1,9 +1,16 @@
+from solution import *
+
+
 def test_sonar_sweep1():
-    data: [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    assert 7
+    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert sonar_sweep(data) == 7
 
 
 def test_sonar_sweep2():
-    data: list(reversed([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]))
-    assert 2
+    data = list(reversed([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]))
+    assert sonar_sweep(data) == 2
 
+
+def test_sonar_sweep_window():
+    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert sonar_sweep_window(data) == 5

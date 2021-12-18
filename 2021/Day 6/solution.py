b79b3ebd0d6e203aea24807784bbc73cc08497ef
Rob <mrrobinhood5@gmail.com>
2021-12-18 21:00:32 +0000

    Day 6 part 2

--- 2021/Day 6/solution.py
+++ 2021/Day 6/solution.py
@@ -1,18 +1,17 @@
-with open('./test_input.txt') as f:
+from collections import deque
+
+with open('input.txt') as f:
     fish_tank = [int(x) for x in f.readline().split(',')]
 
-def do_the_thing(days, fish_tank):
-    while days:
-        temp_tank = []
-        for index, fish in enumerate(fish_tank):
-            # do the checks
-            if not fish: # all 0s go to 6s and adds a new 8
-                fish_tank[index] = 6
-                temp_tank.append(8)
-            else:
-                fish_tank[index] -= 1
-        fish_tank += temp_tank
-        days -= 1
-    return len(fish_tank)
+def do_the_thing_efficiently(days, fish_tank):
+    # build the deque
+    d = deque([fish_tank.count(i) for i in range(0,9)])
+    
+    # play musical chairs
+    for i in range(0, days):
+        d[7] += d[0] if d[0] else 0
+        d.rotate(-1)
+    return(sum(d))
 
-print(do_the_thing(256, fish_tank))
+# print(do_the_thing(256, fish_tank))
+print(do_the_thing_efficiently(256, fish_tank))

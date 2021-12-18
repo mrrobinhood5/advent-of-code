0358afc81ccddd026d44e49c401189053a77ce87
Rob <mrrobinhood5@gmail.com>
2021-12-18 19:49:10 +0000

    i think i fixd git

--- 2021/Day 4/solution.py
+++ 2021/Day 4/solution.py
@@ -2,8 +2,6 @@ with open('./input.txt') as f:
     raw_data = [d.rstrip() for d in f.readlines()]
 
 n = raw_data.pop(0).split(',')
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 def format_raw(raw_data):
     d, t = [], []
@@ -16,46 +14,22 @@ def format_raw(raw_data):
     return d
 
 
-def play_bingo(cards, numbers, flag=0):
-    ci, ni = 0, 0
-    if not flag:
-        c,_, n,_ = checkrows(cards, numbers)
-        return c, int(n)
-    else:
-        while cards:
-            c, ci, n, ni = checkrows(cards, numbers[ni:])
-            cards.pop(ci)
-            if not cards:
-                return c, int(n)
-
-
 def checkrows(cards, numbers):
     ccard = []
-=======
-=======
->>>>>>> 4e0f9f8fc5cbc6d35cb7efdef148921b7d2cd23f
-c, t = [], []
-
-for row in raw_data:
-    if row:
-        t.append(row.split())
-    if len(t) == 5:
-        c.append(t)
-        t = []
-
+    c, t = [], []
+    for row in raw_data:
+        if row:
+            t.append(row.split())
+        if len(t) == 5:
+            c.append(t)
+            t = []
 
 def play_bingo(cards, numbers):
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 4e0f9f8fc5cbc6d35cb7efdef148921b7d2cd23f
     for number in numbers:
         for card in cards:
             for row in card:
                 if number in row:
                     card[card.index(row)][row.index(number)] = 'x'
-<<<<<<< HEAD
-<<<<<<< HEAD
                     ccard = [[card[j][i] for j in range(len(card))] for i in range(len(card[0]))]
                 if ['x']*5 in card or ['x']*5 in ccard:
                     return card, cards.index(card), number, numbers.index(number)
@@ -68,23 +42,3 @@ def find_totals(card, number):
 
 print(f'Day 4, Part1: {find_totals(*play_bingo(format_raw(raw_data), n))}')
 print(f'Day 4, Part2: {find_totals(*play_bingo(format_raw(raw_data), n, flag = 1))}')
-=======
-=======
->>>>>>> 4e0f9f8fc5cbc6d35cb7efdef148921b7d2cd23f
-                if ['x'] * 5 in card:
-                    return card, int(number)
-
-
-def find_totals(card, number):
-    total = 0
-    for row in card:
-        total += sum([int(x) for x in row if x != 'x'])
-    return total * number
-
-# print(play_bingo(c, n))
-<<<<<<< HEAD
-# print(find_totals(*play_bingo(c, n)))
->>>>>>> origin/master
-=======
-# print(find_totals(*play_bingo(c, n)))
->>>>>>> 4e0f9f8fc5cbc6d35cb7efdef148921b7d2cd23f

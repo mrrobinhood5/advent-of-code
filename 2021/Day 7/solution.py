16acadb81101ed8f7f9b48c347e3717bdf90a2fd
Rob <mrrobinhood5@gmail.com>
2021-12-19 17:55:36 +0000

    day 7 part 1 and 2

---
+++ 2021/Day 7/solution.py
@@ -0,0 +1,17 @@
+with open('input.txt') as f:
+    crabs = [int(x) for x in f.readline().split(',')]
+
+def find_min_steps(crabs):
+    counts = []
+    for i in range(min(crabs), max(crabs)):
+        counts.append(sum([abs(x-i) for x in crabs]))
+    return min(counts)
+
+def find_min_steps_exp(crabs):
+    counts = []
+    for i in range(min(crabs), max(crabs)):
+        counts.append(sum([int(abs(x-i)*(abs(x-i)+1)/2) for x in crabs]))
+    return min(counts)
+
+print(find_min_steps(crabs))
+print(find_min_steps_exp(crabs))

1ea0f91b3ef4ad08f7ff00fc86ec1f04f2bfd090
Rob <mrrobinhood5@gmail.com>
2024-12-03 21:49:03 -0600

    AoC 2024 Day 3

--- 2023/01/solution.rb
+++ 2023/01/solution.rb
@@ -5,32 +5,51 @@ require 'minitest/autorun'
 require 'pathname'
 
 class Solution
-  def initialize(type = :example, year = 2024, day = 1)
+  def initialize(type = :example, year = 2024, day = 1, filename)
     @raw_data = if type == :example
-                  load_example(Pathname.new('./test_input.txt'))
+                  if filename.nil?
+                    load_example(Pathname.new('./test_input.txt'))
+                  else
+                    load_example(Pathname.new(filename))
+                  end
                 else
                   load_data(day, year)
                 end
   end
 
   def solution1
-    puts @raw_data.inspect
     r = @raw_data.map do |line|
       x = line.chars.map(&:to_i).reject(&:zero?)
       "#{x.first}#{x.last}".to_i
     end
-    puts r.inspect
     r.sum
   end
 
   def solution2
-    0
+    # TODO: when implementing the parser use the equivalent of .startswith
+    nums = {'one'=> 1, 'two'=> 2, 'three'=> 3, 'four'=> 4, 
+            'five'=> 5, 'six'=> 6 , 'seven'=> 7, 'eight'=> 8, 
+            'nine'=> 9, 'zero'=> 0}
+    r = @raw_data.map do |line|
+      nums.each do |word, digit|
+        line.gsub!(word, digit.to_s)
+      end
+      line
+    end
+    puts r.inspect
+    z = r.map do |line|
+      x = line.chars.map(&:to_i).reject(&:zero?)
+      "#{x.first}#{x.last}".to_i
+    end
+    puts z.inspect
+    z.sum
   end
 end
 
 class ExamplesTest < Minitest::Test
   def setup
-    @solution = Solution.new(:example, 2023, 1)
+    @solution = Solution.new(:example, 2023, 1, './test_input.txt')
+    @solution2 = Solution.new(:example, 2023, 1, './test_input2.txt')
   end
 
   def test_solution_1_example
@@ -38,7 +57,7 @@ class ExamplesTest < Minitest::Test
   end
 
   def test_solution_2_example
-    assert_equal 4, @solution.solution2
+    assert_equal 281, @solution2.solution2
   end
 end
 
---
+++ 2024/03/solution.rb
@@ -0,0 +1,53 @@
+# rubocop:disable Style/Documentation
+
+require '../load_data.rb'
+require 'minitest/autorun'
+require 'pathname'
+
+class Solution
+  def initialize(type = :example, year = 2024, day = 1, filename = './test_input.txt')
+    @raw_data = if type == :example
+                  load_example(Pathname.new(filename))
+                else
+                  load_data(day, year)
+                end
+  end
+
+  def solution1
+    @raw_data.flat_map do |line|
+      line.scan(/mul\((\d+),(\d+)\)/).map do |a, b|
+        a.to_i * b.to_i
+      end
+    end.sum
+  end
+
+  def solution2
+    data = @raw_data.join
+    clearing = /(?=don't\(\))(.*?)(?=do\(\))/
+    matching = /mul\((\d+),(\d+)\)/
+    filtered_data = data.gsub(clearing, "")
+    filtered_data.scan(matching).map do |a, b|
+      a.to_i * b.to_i
+    end.sum
+ 
+  end
+end
+
+class ExamplesTest < Minitest::Test
+  def setup
+    @solution = Solution.new(:example, 2024, 3)
+    @solution2 = Solution.new(:example, 2024, 3, './test_input2.txt')
+  end
+
+  def test_solution_1_example
+    assert_equal 161, @solution.solution1
+  end
+
+  def test_solution_2_example
+    assert_equal 48, @solution2.solution2
+  end
+end
+
+solution = Solution.new(:real, 2024, 3)
+puts solution.solution1.inspect
+puts solution.solution2.inspect

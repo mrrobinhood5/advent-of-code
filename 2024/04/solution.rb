ae53f3658caa725c7a9da44cf555f66c27a5c78f
Rob <mrrobinhood5@gmail.com>
2024-12-05 18:40:10 -0600

    AoC 2024 Day 4 Part 2

--- 2024/04/solution.rb
+++ 2024/04/solution.rb
@@ -14,9 +14,11 @@ class Solution
   def two_side_count(line)
     line.scan(@term).count + line.reverse.scan(@term).count
   end
+
   def transpose_lines(data_set)
     data_set.map(&:chars).transpose.map(&:join)
   end
+
   def get_diagonals(data) 
     rows = data.map(&:chars) 
     diagonals = [] 
@@ -29,6 +31,24 @@ class Solution
     diagonals 
   end
 
+  def undo_diagonals(diagonals)
+    size = (diagonals.size + 1) / 2
+    new_data = Array.new(size) { Array.new(size) }
+
+    diagonals.each_with_index do |line, k|
+      line.chars.each_with_index do |char, i|
+        if k < size
+          new_data[i][k - i] = char
+        else
+          row = k - size + 1 + i
+          col = size - 1 - i
+          new_data[row][col] = char
+        end
+      end
+    end
+    new_data.map(&:join)
+  end
+
   def solution1
     @term = 'XMAS'
     count = 0
@@ -38,8 +58,21 @@ class Solution
     count += get_diagonals(@raw_data.reverse).sum {|line| two_side_count(line)}
   end
 
-  def solution2
+  def find_the_x(data)
+    get_diagonals(data).map do |line|
+        line.gsub(/(?<=M)A(?=S)|(?<=S)A(?=M)/,'1')
+    end
   end
+  
+  def solution2
+    a = undo_diagonals(find_the_x(@raw_data))
+    b = undo_diagonals(find_the_x(@raw_data.reverse)).reverse
+
+    a.zip(b).sum do |a, b|
+      a.chars.zip(b.chars).count {|a, b| a == '1' && b == '1'}
+    end
+  end
+
 end
 
 class ExamplesTest < Minitest::Test
@@ -53,10 +86,10 @@ class ExamplesTest < Minitest::Test
   end
 
   def test_solution_2_example
-    assert_equal 48, @solution2.solution2
+    assert_equal 9, @solution2.solution2
   end
 end
 
 solution = Solution.new(:real, 2024, 4)
 puts solution.solution1.inspect
-# puts solution.solution2.inspect
+puts solution.solution2.inspect
